6a2ee4aa25c0de5aea6dd7e54fd5b6987c193d25
HiPeng-sange <51777997+HiPeng-sange@users.noreply.github.com>
2020-08-19 09:13:35 +0700

    First Commit, mubal

---
+++ app/src/main/java/com/losdol/prm391x_quizapp_hidayatmhyfx07085/quizMain.java
@@ -0,0 +1,14 @@
+package com.losdol.prm391x_quizapp_hidayatmhyfx07085;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quizMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_main);
+    }
+}
